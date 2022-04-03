62521b2b78ddf933084b55868e200bd2807715d9
Sinsl <sinsl@me.com>
2022-04-03 20:49:33 +0300

    Пункт задания 5. Добавление ProfileHeaderView

--- Navigation/ProfileViewController.swift
+++ Navigation/ProfileViewController.swift
@@ -9,10 +9,19 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    var profileHeaderView = ProfileHeaderView(frame: .zero)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .systemGray3
+        self.view.addSubview(profileHeaderView)
+        
     }
-
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        profileHeaderView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+       
+    }
+    
 }
