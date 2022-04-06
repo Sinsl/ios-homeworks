fc602cf9e1f93a69d33e9067df60e53d26095675
Sinsl <sinsl@me.com>
2022-04-06 16:38:50 +0300

    Дополнительное задание. Добавление в FeedViewController двух кнопок

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -11,39 +11,63 @@ class FeedViewController: UIViewController {
 
     var post = Post(title: "Мой пост")
     
-    private lazy var button: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .init(red: 11/255, green: 215/255, blue: 55/255, alpha: 1)
-        button.layer.cornerRadius = 8
-        button.layer.borderWidth = 2
-        button.layer.borderColor = .init(red: 12/255, green: 115/255, blue: 34/255, alpha: 1)
-        button.setTitle("Выбранный пост", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
-        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
-    return button
+    private lazy var stackView: UIStackView = {
+        let view = UIStackView()
+//        view.backgroundColor = .systemGray5
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.axis = .vertical
+        view.spacing = 10
+        view.distribution = .fillEqually
+        return view
+    }()
+    private lazy var buttonOne: UIButton = {
+       let button = UIButton()
+        button.backgroundColor = .systemGreen
+        button.setTitle("Кнопка один", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 14
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        return button
+    }()
+    private lazy var buttonTwo: UIButton = {
+       let button = UIButton()
+        button.backgroundColor = .systemRed
+        button.setTitle("Кнопка два", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 14
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBlue
-        setupButton()
+        self.view.addSubview(stackView)
+        setLoyaout()
+        setButtons()
+        
     }
     
-    @objc private func buttonAction() {
+    private func setLoyaout(){
+        NSLayoutConstraint.activate([
+            stackView.heightAnchor.constraint(equalToConstant: 300),
+            stackView.widthAnchor.constraint(equalToConstant: 300),
+            stackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
+    }
+    private func setButtons() {
+        stackView.addArrangedSubview(buttonOne)
+        stackView.addArrangedSubview(buttonTwo)
+    }
+    
+    @objc private func buttonPressed() {
         let postViewController = PostViewController()
+
         self.navigationController?.pushViewController(postViewController, animated: true)
         postViewController.titlePost = post.title
-        }
-    
-    private func setupButton() {
-        self.view.addSubview(self.button)
-        
-        self.button.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        self.button.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 30).isActive = true
-        self.button.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -30).isActive = true
-        self.button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
-        }
+    }
 }
