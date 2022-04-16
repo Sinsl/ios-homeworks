9fefcde5799fb85394823e402a6ff5e9f22765d7
Sinsl <sinsl@me.com>
2022-04-16 21:49:22 +0300

    Пункты 3-5. Определение tableView

--- Navigation/ProfileViewController.swift
+++ Navigation/ProfileViewController.swift
@@ -9,53 +9,45 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
-    var profileHeaderView = ProfileHeaderView(frame: .zero)
-    
-    private lazy var newButton: UIButton = {
-
-            let button = UIButton()
-                button.backgroundColor = .systemBlue
-                button.setTitle("Новая кнопка", for: .normal)
-                button.setTitleColor(.white, for: .normal)
-                button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
-//                button.addTarget(self, action: #selector( ), for: .touchUpInside)
-                button.translatesAutoresizingMaskIntoConstraints = false
-
-            return button
-        }()
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        tableView.delegate = self
+//        tableView.register(PostTableViewCell.self, forCellReuseIdentifier: "PostTableViewCell")
+        return tableView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationController?.navigationBar.isHidden = false
-        view.backgroundColor = .systemGray4
-        self.view.addSubview(profileHeaderView)
-        self.view.addSubview(newButton)
-        setLayout()
         
+        view.backgroundColor = .white
+        setLayout()
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        self.navigationController?.navigationBar.isHidden = true
-    }
     
     func setLayout() {
-        profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
         NSLayoutConstraint.activate([
-           profileHeaderView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0),
-           profileHeaderView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0),
-           profileHeaderView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 0),
-           profileHeaderView.heightAnchor.constraint(equalToConstant: 220)
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
-        NSLayoutConstraint.activate([
-            newButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0),
-            newButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0),
-            newButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
-            newButton.heightAnchor.constraint(equalToConstant: 50)
-        ])
-        
+    }
+}
+
+
+extension ProfileViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
     }
     
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+}
+extension ProfileViewController: UITableViewDelegate {
+    
 }
 
